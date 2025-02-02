6606783bc92e7223584e35b247e7b51f3c818b8a
mine702 <mine702@naver.com>
2025-02-02 22:34:09 +0900

    인증 – Authentication

--- springsecuritymaster/src/main/java/io/security/springsecuritymaster/SecurityConfig.java
+++ springsecuritymaster/src/main/java/io/security/springsecuritymaster/SecurityConfig.java
@@ -1,24 +1,17 @@
 package io.security.springsecuritymaster;
 
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
-import org.springframework.security.web.savedrequest.SavedRequest;
-
-import java.io.IOException;
 
 @Slf4j
 @EnableWebSecurity
@@ -27,8 +20,7 @@ public class SecurityConfig {
 
     /**
      * basic formLogin()
-     Bean
-     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+     @Bean public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
      http
      .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
      .formLogin(form -> form
@@ -56,80 +48,132 @@ public class SecurityConfig {
     /**
      * rememberMe()
      *
-     * @Bean public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
-     * http
-     * .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
-     * .formLogin(Customizer.withDefaults())
-     * .rememberMe(remeberMe -> remeberMe
-     * .alwaysRemember(true)
-     * .tokenValiditySeconds(3600)
-     * .userDetailsService(userDetailsService())
-     * .rememberMeParameter("remember")
-     * .rememberMeCookieName("remember")
-     * .key("security")
-     * );
-     * <p>
-     * return http.build();
-     * }
+     @Bean public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+     http
+     .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+     .formLogin(Customizer.withDefaults())
+     .rememberMe(remeberMe -> remeberMe
+     .alwaysRemember(true)
+     .tokenValiditySeconds(3600)
+     .userDetailsService(userDetailsService())
+     .rememberMeParameter("remember")
+     .rememberMeCookieName("remember")
+     .key("security")
+     );
+     return http.build();
+     }
      */
 
     /**
      * anonymous()
      *
-     * @Bean public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-     * http
-     * .authorizeHttpRequests(auth -> auth
-     * .requestMatchers("/anonymous").hasRole("GUEST")
-     * .requestMatchers("/anonymousContext", "/authentication").permitAll()
-     * .anyRequest()
-     * .authenticated())
-     * .formLogin(Customizer.withDefaults())
-     * .anonymous(anonymous -> anonymous
-     * .principal("guest")
-     * .authorities("ROLE_GUEST")
-     * )
-     * ;
-     * return http.build();
-     * }
+     @Bean public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+     http
+     .authorizeHttpRequests(auth -> auth
+     .requestMatchers("/anonymous").hasRole("GUEST")
+     .requestMatchers("/anonymousContext", "/authentication").permitAll()
+     .anyRequest()
+     .authenticated())
+     .formLogin(Customizer.withDefaults())
+     .anonymous(anonymous -> anonymous
+     .principal("guest")
+     .authorities("ROLE_GUEST")
+     )
+     ;
+     return http.build();
+     }
      */
 
     /**
-     * logout2
-     *
-     * @Bean public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-     * http
-     * .authorizeHttpRequests(auth -> auth
-     * .requestMatchers("/logoutSuccess").permitAll()
-     * .anyRequest()
-     * .authenticated())
-     * .formLogin(Customizer.withDefaults())
-     * .logout(logout -> logout
-     * .logoutUrl("/logoutProc")
-     * .logoutRequestMatcher(new AntPathRequestMatcher("/logoutProc", "POST"))
-     * .logoutSuccessUrl("/logoutSuccess")
-     * .logoutSuccessHandler(new LogoutSuccessHandler() {
-     * @Override public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
-     * response.sendRedirect("/logoutSuccess");
-     * }
-     * })
-     * .invalidateHttpSession(true)
-     * .deleteCookies("rememberMe", "JSESSIONID")
-     * .clearAuthentication(true)
-     * .addLogoutHandler(new LogoutHandler() {
-     * @Override public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
-     * HttpSession session = request.getSession();
-     * session.invalidate();
-     * SecurityContextHolder.getContextHolderStrategy().getContext().setAuthentication(null);
-     * SecurityContextHolder.getContextHolderStrategy().clearContext();
-     * }
-     * })
-     * .permitAll()
-     * )
-     * ;
-     * return http.build();
-     * }
+     logout2
+
+     @Bean public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+     http
+     .authorizeHttpRequests(auth -> auth
+     .requestMatchers("/logoutSuccess").permitAll()
+     .anyRequest()
+     .authenticated())
+     .formLogin(Customizer.withDefaults())
+     .logout(logout -> logout
+     .logoutUrl("/logoutProc")
+     .logoutRequestMatcher(new AntPathRequestMatcher("/logoutProc", "POST"))
+     .logoutSuccessUrl("/logoutSuccess")
+     .logoutSuccessHandler(new LogoutSuccessHandler() {
+     @Override public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+     response.sendRedirect("/logoutSuccess");
+     }
+     })
+     .invalidateHttpSession(true)
+     .deleteCookies("rememberMe", "JSESSIONID")
+     .clearAuthentication(true)
+     .addLogoutHandler(new LogoutHandler() {
+     @Override public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
+     HttpSession session = request.getSession();
+     session.invalidate();
+     SecurityContextHolder.getContextHolderStrategy().getContext().setAuthentication(null);
+     SecurityContextHolder.getContextHolderStrategy().clearContext();
+     }
+     })
+     .permitAll()
+     )
+     ;
+     return http.build();
+     }
+     /
+
+     /**
+     요청 캐시
+     @Bean public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+     <p>
+     HttpSessionRequestCache requestCache = new HttpSessionRequestCache();
+     requestCache.setMatchingRequestParameterName("customParam=y");
+     <p>
+     http
+     .authorizeHttpRequests(auth -> auth
+     .requestMatchers("/logoutSuccess").permitAll()
+     .anyRequest()
+     .authenticated())
+     .formLogin(form -> form
+     .successHandler(new AuthenticationSuccessHandler() {
+     @Override public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+     SavedRequest savedRequest = requestCache.getRequest(request, response);
+     String redirectUrl = savedRequest.getRedirectUrl();
+     response.sendRedirect(redirectUrl);
+     }
+     })
+     ).requestCache(cache -> cache.requestCache(requestCache))
+     ;
+     return http.build();
+     }
      */
 
+    /**
+     요청 캐시
+
+     @Bean public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+     HttpSessionRequestCache requestCache = new HttpSessionRequestCache();
+     requestCache.setMatchingRequestParameterName("customParam=y");
+
+     http
+     .authorizeHttpRequests(auth -> auth
+     .requestMatchers("/logoutSuccess").permitAll()
+     .anyRequest()
+     .authenticated())
+     .formLogin(form -> form
+     .successHandler(new AuthenticationSuccessHandler() {
+     @Override public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+     SavedRequest savedRequest = requestCache.getRequest(request, response);
+     String redirectUrl = savedRequest.getRedirectUrl();
+     response.sendRedirect(redirectUrl);
+     }
+     })
+     ).requestCache(cache -> cache.requestCache(requestCache))
+     ;
+     return http.build();
+     }
+     */
+    
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
@@ -137,21 +181,13 @@ public class SecurityConfig {
         requestCache.setMatchingRequestParameterName("customParam=y");
 
         http
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/logoutSuccess").permitAll()
-                        .anyRequest()
-                        .authenticated())
-                .formLogin(form -> form
-                        .successHandler(new AuthenticationSuccessHandler() {
-                            @Override
-                            public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
-                                SavedRequest savedRequest = requestCache.getRequest(request, response);
-                                String redirectUrl = savedRequest.getRedirectUrl();
-                                response.sendRedirect(redirectUrl);
-                            }
-                        })
-                ).requestCache(cache -> cache.requestCache(requestCache))
-        ;
+                .authorizeHttpRequests(
+                        auth -> auth
+                                .requestMatchers("/logoutSuccess").permitAll()
+                                .anyRequest().authenticated())
+                .formLogin(Customizer.withDefaults())
+                .httpBasic(Customizer.withDefaults())
+                .userDetailsService(userDetailsService());
         return http.build();
     }
 
