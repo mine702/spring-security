1031d0113d8fd4e553917a1d46fde32f1f1d5bd0
mine702 <mine702@naver.com>
2025-03-04 23:34:41 +0900

    인증 컨텍스트 - SecurityContext / SecurityContextHolder -2 (2)

--- springsecuritymaster/src/main/java/io/security/springsecuritymaster/IndexController.java
+++ springsecuritymaster/src/main/java/io/security/springsecuritymaster/IndexController.java
@@ -1,21 +1,30 @@
 package io.security.springsecuritymaster;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.CurrentSecurityContext;
 import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
+@RequiredArgsConstructor
 public class IndexController {
+
+    private final SecurityContextService securityContextService;
+
     @GetMapping("/")
-    public String index(String customParam) {
-        if (customParam != null) {
-            return "customPage";
-        } else {
-            return "index";
-        }
+    public String index() {
+        SecurityContext context = SecurityContextHolder.getContextHolderStrategy().getContext();
+        Authentication authentication = context.getAuthentication();
+        log.info("authentication = {}", authentication);
+
+        securityContextService.securityContext();
+        return "index";
     }
 
     @GetMapping("/home")
---
+++ springsecuritymaster/src/main/java/io/security/springsecuritymaster/SecurityContextService.java
@@ -0,0 +1,12 @@
+package io.security.springsecuritymaster;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SecurityContextService {
+    public void securityContext() {
+        // SecurityContext context = SecurityContextHolder.getContextHolderStrategy().getContext();
+        // Authentication authentication = context.getAuthentication();
+        // log.info("authentication = {}", authentication);
+    }
+}
