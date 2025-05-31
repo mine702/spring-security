5c5bece5963824b542e86b327bbda674e5a37e38
mine702 <mine702@naver.com>
2025-05-31 12:16:26 +0900

    인증 관리자 - AuthenticationManager - 2

--- springsecuritymaster/src/main/java/io/security/springsecuritymaster/SecurityConfig.java
+++ springsecuritymaster/src/main/java/io/security/springsecuritymaster/SecurityConfig.java
@@ -3,7 +3,8 @@ package io.security.springsecuritymaster;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -11,7 +12,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Slf4j
 @EnableWebSecurity
@@ -194,20 +195,44 @@ public class SecurityConfig {
      }
      */
 
+    /**
+     인증 관리자 - 1
+
+     @Bean public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+     HttpSessionRequestCache requestCache = new HttpSessionRequestCache();
+     requestCache.setMatchingRequestParameterName("customParam=y");
+
+     http
+     .authorizeHttpRequests(
+     auth -> auth
+     .requestMatchers("/logoutSuccess").permitAll()
+     .anyRequest().authenticated())
+     .formLogin(Customizer.withDefaults()
+     );
+     return http.build();
+     }
+     */
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-
-        HttpSessionRequestCache requestCache = new HttpSessionRequestCache();
-        requestCache.setMatchingRequestParameterName("customParam=y");
+        AuthenticationManagerBuilder authenticationManagerBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
+        AuthenticationManager authenticationManager = authenticationManagerBuilder.build();
+        //AuthenticationManager authenticationManager = authenticationManagerBuilder.getObject();
 
         http
-                .authorizeHttpRequests(
-                        auth -> auth
-                                .requestMatchers("/logoutSuccess").permitAll()
-                                .anyRequest().authenticated())
-                .formLogin(Customizer.withDefaults()
-                );
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/login").permitAll()
+                        .anyRequest().authenticated())
+                .authenticationManager(authenticationManager)
+                .addFilterBefore(customFilter(http, authenticationManager), UsernamePasswordAuthenticationFilter.class);
         return http.build();
+    }
+
+    public CustomAuthenticationFilter customFilter(HttpSecurity http, AuthenticationManager authenticationManager) throws Exception {
+        CustomAuthenticationFilter customAuthenticationFilter = new CustomAuthenticationFilter(http);
+        customAuthenticationFilter.setAuthenticationManager(authenticationManager);
+        return customAuthenticationFilter;
     }
 
     @Bean
